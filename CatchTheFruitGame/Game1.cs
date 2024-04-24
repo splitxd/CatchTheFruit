18fbf8133b9bb9ac4f1cec01082dce8d6ba0d0f5
splitxd <d4n4tik@gmail.com>
2024-04-24 20:15:10 +0500

    v4
    Добавил звуки при ловле бомб и фруктов, звуки при наборе 10,100 и 1000 очков, добавил меняющийся в размерах фон для "score: __", выделил методы и остальное по мелочи

--- CatchTheFruitGame/Game1.cs
+++ CatchTheFruitGame/Game1.cs
@@ -1,47 +1,55 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
 
+
 namespace CatchTheFruitGame
 {
-    public class Object
-    {
-        public Texture2D Texture { get; private set; }
-        public Vector2 Position { get; private set; }
 
-        public Object(Texture2D texture, Vector2 position)
-        {
-            Texture = texture;
-            Position = position;
-        }
-    }
     public class Game1 : Game
     {
-        //Тарелка
+        //Звуки
+        static Song crunchSound_1;
+        static Song crunchSound_2;
+        static Song crunchSound_3;
+        static List<Song> crunchSounds;
+        static Song tenPoints;
+        static Song hundretPoints;
+        static Song thousenPoints;
+
+        static Song bombSound;
+
+        //Текстуры
         Texture2D plateTexture;
-        Vector2 platePosition;
-        float defaultSpeed = 300f;
-        float plateSpeed;
-        const int plateSize = 100;
-
-
-        //Фрукты
         Texture2D melonTexture;
         Texture2D appleTexture;
         Texture2D peachTexture;
         Texture2D orangeTexture;
+        static Texture2D bombTexture;
+        Texture2D background;
+        Texture2D whiteTexture;
+
+        //Тарелка
+        static Vector2 platePosition;
+        static float defaultSpeed = 300f;
+        static float plateSpeed;
+        const int plateSize = 100;
+
+
+        //Фрукты
         static List<Texture2D> fruitTextures = new List<Texture2D>();
         const float fruitSpeed = 200f;
         const int fruitSize = 64;
         private static List<Object> objPositions = new List<Object>();
 
-        //Бомба
-        static Texture2D bombTexture;
-
         //Очки
         static int score;
+        static int backRectWidth = 152;
+        static int backRectHeight = 46;
 
         //Переменные
         static Random rnd = new Random();
@@ -50,7 +58,7 @@ namespace CatchTheFruitGame
 
 
         //Отображение и логические переменные
-        Texture2D background;
+        
         private SpriteFont font;
         private static GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -59,6 +67,11 @@ namespace CatchTheFruitGame
         private TimeSpan elapsedTimeSinceLastFruit;
         int fruitSpawnTime = 1;
 
+        //для звуков 
+        static int previous = 0;
+        static bool isFirstTen = true;
+        static bool isFirtstHundred = true;
+        static bool isFirstTousand = true;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -71,6 +84,9 @@ namespace CatchTheFruitGame
             _graphics.PreferredBackBufferHeight = 600;
             _graphics.PreferredBackBufferWidth = 800;
             _graphics.ApplyChanges();
+            crunchSounds = new List<Song>();
+            whiteTexture = new Texture2D(GraphicsDevice, 1, 1);
+            whiteTexture.SetData(new[] { Color.White });
             score = 0;
             plateSpeed = defaultSpeed;
 
@@ -94,36 +110,25 @@ namespace CatchTheFruitGame
             bombTexture = Content.Load<Texture2D>("bomb");
             plateTexture = Content.Load<Texture2D>("plate");
             font = Content.Load<SpriteFont>("File");
-
+            bombSound = Content.Load<Song>("bombSound");
+            crunchSound_1 = Content.Load<Song>("crunchSound_1");
+            crunchSound_2 = Content.Load<Song>("crunchSound_2");
+            crunchSound_3 = Content.Load<Song>("crunchSound_3");
+            crunchSounds.Add(crunchSound_1);
+            crunchSounds.Add(crunchSound_2);
+            crunchSounds.Add(crunchSound_3);
+            tenPoints = Content.Load<Song>("10points");
+            hundretPoints = Content.Load<Song>("100points");
+            thousenPoints = Content.Load<Song>("1000points");
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            var kstate = Keyboard.GetState();
 
-            //Передвижение тарелки
-            if (kstate.IsKeyDown(Keys.A) || kstate.IsKeyDown(Keys.Left))
-            {
-                if (kstate.IsKeyDown(Keys.LeftShift))
-                    plateSpeed = defaultSpeed * 1.5f;
-                else
-                    plateSpeed = defaultSpeed;
-                platePosition.X -= plateSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-            if (kstate.IsKeyDown(Keys.D) || kstate.IsKeyDown(Keys.Right))
-            {
-                if (kstate.IsKeyDown(Keys.LeftShift))
-                    plateSpeed = defaultSpeed * 1.5f;
-                else
-                    plateSpeed = defaultSpeed;
-                platePosition.X += plateSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-            if (platePosition.X <= 0)
-                platePosition.X = 0;
-            else if (platePosition.X >= _graphics.PreferredBackBufferWidth - plateSize)
-                platePosition.X = _graphics.PreferredBackBufferWidth - plateSize;
+            Movement(gameTime);
+
 
             //Спавн объектов по таймеру
             elapsedTimeSinceLastFruit += gameTime.ElapsedGameTime;
@@ -145,8 +150,80 @@ namespace CatchTheFruitGame
 
                 Colluzion(platePosition, objPositions[i], i);
             }
+            ScoreSounds();
 
             base.Update(gameTime);
+        }
+
+        public static void Movement(GameTime gameTime)
+        {
+            var kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.A) || kstate.IsKeyDown(Keys.Left))
+            {
+                if (kstate.IsKeyDown(Keys.LeftShift))
+                    plateSpeed = defaultSpeed * 1.5f;
+                else
+                    plateSpeed = defaultSpeed;
+                platePosition.X -= plateSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            if (kstate.IsKeyDown(Keys.D) || kstate.IsKeyDown(Keys.Right))
+            {
+                if (kstate.IsKeyDown(Keys.LeftShift))
+                    plateSpeed = defaultSpeed * 1.5f;
+                else
+                    plateSpeed = defaultSpeed;
+                platePosition.X += plateSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            if (platePosition.X <= 0)
+                platePosition.X = 0;
+            else if (platePosition.X >= _graphics.PreferredBackBufferWidth - plateSize)
+                platePosition.X = _graphics.PreferredBackBufferWidth - plateSize;
+        }
+
+        public static void ScoreSounds()
+        {
+            if (score % 1000 == 0 && score != 0)
+            {
+                if (previous < score)
+                {
+                    MediaPlayer.Play(thousenPoints);
+                    previous = score;
+                }
+                if (isFirstTousand)
+                {
+                    var temp = backRectWidth + 20;
+                    backRectWidth = temp;
+                    isFirstTousand = false;
+                }
+            }
+            else if (score % 100 == 0 && score != 0)
+            {
+                if (previous < score)
+                {
+                    MediaPlayer.Play(hundretPoints);
+                    previous = score;
+                }
+                if (isFirtstHundred)
+                {
+                    var temp = backRectWidth + 20;
+                    backRectWidth = temp;
+                    isFirtstHundred = false;
+                }
+            }
+            else if (score % 10 == 0 && score != 0)
+            {
+                if (previous < score)
+                {
+                    MediaPlayer.Play(tenPoints);
+                    previous = score;
+                }
+                if (isFirstTen)
+                {
+                    var temp = backRectWidth + 20;
+                    backRectWidth = temp;
+                    isFirstTen = false;
+                }
+            }
         }
 
         //Столкновение с объектами
@@ -158,17 +235,26 @@ namespace CatchTheFruitGame
             {
 
                 if (obj.Texture != bombTexture)
+                {
                     score--;
+                }
                 objPositions.RemoveAt(i);
             }
-            else if (fruitRect.Intersects(plateRect))
+            else if (fruitRect.Intersects(plateRect) && (plate.Y + (plateSize / 2) > obj.Position.Y ))
             {
                 if (obj.Texture == bombTexture)
+                {
+                    MediaPlayer.Play(bombSound);
                     score--;
+                }
                 else
+                {
+                    MediaPlayer.Play(crunchSounds[rnd.Next(0, crunchSounds.Count)]);
                     score++;
+                }
                 objPositions.RemoveAt(i);
             }
+            
         }
 
         //Добавление объектов в список
@@ -193,25 +279,26 @@ namespace CatchTheFruitGame
             return new Vector2(_graphics.PreferredBackBufferWidth / 2 - fruitSize / 2 + rnd.Next(-300, 300), -(int)fruitSize);
         }
         //Изменение размеров объектов
-        public static Rectangle ChangeSizeRect(Vector2 position, int size)
+        public static Rectangle ChangeSizeRect(Vector2 position, int width, int height)
         {
-            return new Rectangle((int)position.X, (int)position.Y, size, size);
+            return new Rectangle((int)position.X, (int)position.Y, width, height);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.White);
             _spriteBatch.Begin();
-            _spriteBatch.Draw(background, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
-            _spriteBatch.Draw(plateTexture, ChangeSizeRect(platePosition, plateSize), Color.LightGray);
+            _spriteBatch.Draw(background, ChangeSizeRect(Vector2.Zero, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
+            _spriteBatch.Draw(plateTexture, ChangeSizeRect(platePosition, plateSize,plateSize), Color.LightGray);
 
             // Отрисовка объектов
             foreach (var fruitPosition in objPositions)
             {
-                _spriteBatch.Draw(fruitPosition.Texture, ChangeSizeRect(fruitPosition.Position, fruitSize), Color.White);
+                _spriteBatch.Draw(fruitPosition.Texture, ChangeSizeRect(fruitPosition.Position, fruitSize,fruitSize), Color.White);
             }
 
-            _spriteBatch.DrawString(font, "Score: " + score.ToString(), new Vector2(10, 20), Color.White);
+            _spriteBatch.Draw(whiteTexture, ChangeSizeRect(new Vector2(8, 18), backRectWidth, backRectHeight), new Color(34, 139, 34));
+            _spriteBatch.DrawString(font, "Score: " + score.ToString(), new Vector2(10, 20), new Color(173, 255, 47));
 
             _spriteBatch.End();
 
---
+++ CatchTheFruitGame/Object.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+public class Object
+{
+    public Texture2D Texture { get; private set; }
+    public Vector2 Position { get; private set; }
+
+    public Object(Texture2D texture, Vector2 position)
+    {
+        Texture = texture;
+        Position = position;
+    }
+}
